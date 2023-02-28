e78d5e9b9eecd2e05a5e91095e4372d130379dfe
newtonjob <jobnewton3@gmail.com>
2023-02-28 15:23:28 -0800

    Register the api response macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerResponseMacros();
+    }
+
+    public function registerResponseMacros(): void
+    {
+        Response::macro('api', function ($response, $status = 200) {
+            $format = ['status' => ($status < 400), 'title' => '', 'message' => '', 'data' => []];
+
+            // For convenience, if $response is a string, we'll use it as the message...
+            if (! is_array($response)) $response = ['message' => $response];
+
+            return response(array_merge($format, $response), $status);
+        });
     }
 }
